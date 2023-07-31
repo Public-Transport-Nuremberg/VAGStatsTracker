484fff0bf8395073c095f6881fda53da3e36c0aa
BolverBlitz <bolverblitz@ebg.pw>
2023-07-31 10:28:37 +0200

    Refactor

--- FahrtenScanner/index.js
+++ FahrtenScanner/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('module-alias/register');
 const watchdog = require('@lib/watchdog');
 const { log } = require('@lib/logger');
+const joi = require('joi');
 
 process.app = {};
 process.app.watchdog = watchdog;
