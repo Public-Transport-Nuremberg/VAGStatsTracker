7c11f9a9b5d2a0df2cb63e8054c1dc6452705924
BolverBlitz <bolverblitz@ebg.pw>
2024-03-10 11:21:37 +0100

    Added processor (kinda working)

--- FahrtenScanner/src/index.js
+++ FahrtenScanner/src/index.js
@@ -20,6 +20,13 @@ const MakeTripRequests = async () => {
             const { status, value } = result;
             if (status === 'rejected') return;
 
+            // Check if value is instance of Error
+            if (value instanceof Error) {
+                process.log.error(value);
+                writeNewDatapoint('Trips.Error', value.code) // Log the error code
+                return;
+            }
+
             const { Fahrt, Meta } = value;
 
             writeNewDatapoint('Trips.RequestTime', Meta.RequestTime); // Store the request time for later analysis
@@ -30,11 +37,21 @@ const MakeTripRequests = async () => {
             const filteredFahrten = await filterDuplicates(Fahrten); // Check for duplicates we already have in the queue
             process.log.debug(`Filtered ${Fahrten.length - filteredFahrten.length} duplicates for ${Produkt}`);
 
+            //Limit it at 1 to test the system
+            // const testfilteredFahrten = filteredFahrten.slice(0, 1)
+
             filteredFahrten.map(async (fahrt) => {
                 const { Fahrtnummer, Betriebstag } = fahrt;
-                const abfaht = await vgn.getTrip(Fahrtnummer, { product: Produkt, date: Betriebstag })
+                const abfahrt = await vgn.getTrip(Fahrtnummer, { product: Produkt, date: Betriebstag })
 
-                const { Fahrt, Meta } = abfaht;
+                // Check if value is instance of Error
+                if (abfahrt instanceof Error) {
+                    process.log.error(abfahrt);
+                    writeNewDatapoint('Trip.Error', abfahrt.code) // Log the error code
+                    return;
+                }
+
+                const { Fahrt, Meta } = abfahrt;
                 const { Fahrtverlauf } = Fahrt;
                 writeNewDatapoint('Trip.RequestTime', Meta.RequestTime); // Store the request time for later analysis
 
@@ -43,13 +60,14 @@ const MakeTripRequests = async () => {
                 if (futureIndex === -1) return;
 
                 const TripTimeline = Fahrtverlauf.map((stop) => { return stop.VGNKennung }); // Get the timeline of the trip, so we can recreate the job without another trip request
+                const TripDepartureTimeline = Fahrtverlauf.map((stop) => { return new Date(stop.AnkunftszeitSoll || stop.AbfahrtszeitSoll).getTime() - parseInt(process.env.SCANBEFORE, 10) || 5 }); // Get the timeline of the trip, so we can recreate the job without another trip request
 
                 const futureFahrt = Fahrtverlauf[futureIndex]; // Get data for the next stop in the future
                 futureFahrt.Fahrtnummer = Fahrtnummer; // Store that so we can filter the exact trip later
                 futureFahrt.Linienname = Fahrt.Linienname; // Store that so we can filter the exact trip later
                 const timestamp = new Date(futureFahrt.AnkunftszeitSoll || futureFahrt.AbfahrtszeitSoll).getTime();
 
-                ScheduleJob(Fahrtnummer, futureFahrt, TripTimeline, timestamp) // Create the job and a key to use for filtering
+                ScheduleJob(Fahrtnummer, futureFahrt, TripTimeline, TripDepartureTimeline, timestamp) // Create the job and a key to use for filtering
             });
         });
 
