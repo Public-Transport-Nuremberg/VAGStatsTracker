6146c0b6c2b915f6ea7e2e7a8f92a6d87ae18101
BolverBlitz <bolverblitz@ebg.pw>
2024-04-01 23:17:57 +0200

    Create heatmap.js

---
+++ WebService/public/dist/js/heatmap.js
@@ -0,0 +1,59 @@
+const getYesterdaysDate = () => {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0');
+    const yyyy = date.getFullYear();
+
+    return `${yyyy}-${mm}-${dd}`;
+}
+
+const map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([11.1131575166207, 49.487582179245]),
+        zoom: 13
+    })
+});
+
+const heatmapLayer = new ol.layer.Heatmap({
+    source: new ol.source.Vector(),
+    blur: parseInt(15, 10),
+    radius: parseInt(25, 10),
+});
+
+map.addLayer(heatmapLayer);
+
+let queryString = window.location.search;
+
+// If queryString is empty, fetch yesterday's data ?at=yyyy-mm-dd
+queryString === '' ? queryString = `?at=${getYesterdaysDate()}` : queryString;
+
+fetch(`http://localhost/api/v1/heatmap${queryString}`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        var minDelayLog = Math.log(Math.min(...data.map(item => Math.max(item.avg_delay, 1))));
+        var maxDelayLog = Math.log(Math.max(...data.map(item => Math.max(item.avg_delay, 1))));
+        var features = data.map(function(point) {
+            var delayLog = Math.log(Math.max(point.avg_delay, 1));
+            var normalizedWeight = (delayLog - minDelayLog) / (maxDelayLog - minDelayLog);
+            var feature = new ol.Feature({
+                geometry: new ol.geom.Point(ol.proj.fromLonLat([point.longitude, point.latitude])),
+                weight: normalizedWeight * normalizedWeight * normalizedWeight
+            });
+            return feature;
+        });
+
+        heatmapLayer.getSource().addFeatures(features);
+    })
+    .catch(function(error) {
+        console.log('Error fetching or parsing data:', error);
+    });
