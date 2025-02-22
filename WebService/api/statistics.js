339956e170b7b51027a13109fddf14ad659a5726
Marc <bolverblitz@ebg.pw>
2025-02-22 02:45:47 +0100

    Improved UI from linestats

--- WebService/api/statistics.js
+++ WebService/api/statistics.js
@@ -15,6 +15,10 @@ const PluginName = 'Statisics'; //This plugins name
 const PluginRequirements = []; //Put your Requirements and version here <Name, not file name>|Version
 const PluginVersion = '0.0.1'; //This plugins version
 
+router.get('/lines', limiter(), async (req, res) => {
+    res.json(Object.keys(linesWithStops));
+});
+
 router.get('/delay/line', limiter(), async (req, res) => {
     const value = await delayLineSchema.validateAsync(req.query);
     if (value.error) {
